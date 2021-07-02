e3feec8ee63364aa73781f9c95318690a16741ac
WiktorGajewski <66271811+WiktorGajewski@users.noreply.github.com>
2021-07-02 23:47:37 +0200

    Implemented drawRandomLineFromFile - picking random capital from file

--- HangmanGame/Hangman.cs
+++ HangmanGame/Hangman.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace HangmanGame
 {
@@ -11,30 +11,53 @@ namespace HangmanGame
         {
             lifePoints = 5;
         }
+        private string drawRandomLineFromFile()
+        {
+            string filePath = "Assets/countries_and_capitals.txt";
+            if(File.Exists(filePath))
+            {
+                int linesCount = File.ReadLines(filePath).Count();
+                var random = new Random();
+                int randomNumber = random.Next(1, linesCount);
+
+                string line = File.ReadLines(filePath).Skip(--randomNumber).Take(1).First();
+                return line;
+            }
+            else
+            {
+                Console.WriteLine($"File '{filePath}' could not be found");
+                throw new FileNotFoundException();
+            }
+        }
         private bool NewRound()
         {
-            Console.WriteLine("[------------- HANGMAN -------------]");
-            bool solved = false;
+            Console.WriteLine("[------------- \tHANGMAN\t -------------]");
+            bool quessedWord = false;
+            lifePoints = 5;
 
-            while(lifePoints>0 || solved)
+            string[] solution = drawRandomLineFromFile().Split(" | ");
+            string country = solution[0];
+            string capital = solution[1];
+            
+            while(lifePoints>0 || quessedWord)
             {
 
                 //quessing
-                solved = true;
+                quessedWord = true;
 
-                if(lifePoints==0)
+                if(lifePoints<=0)
                 {
                     Console.WriteLine("You lose");
                     break;
                 } 
-                else if(solved)
+                else if(quessedWord)
                 {
                     Console.WriteLine("You win");
                     break;
                 }
             }
 
-            for (; ; )
+            for (; ; )  //question about restarting program
             {
                 Console.WriteLine("Do you want to give it another try? (y/n)");
                 string keepPlaying = Console.ReadLine();
@@ -44,7 +67,7 @@ namespace HangmanGame
                     return false;
                 else
                     Console.WriteLine("Please use only 'y' or 'n'");
-            }
+            }           //
         }
         public void StartGame()
         {
